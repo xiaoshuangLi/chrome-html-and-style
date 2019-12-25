6739b789c9747bd5d2a679b7364c0cb4b667eadb
xiaoshuang.li <qq1157502759@gmail.com>
2019-12-25 17:31:08 +0800

    fix: 🐛 get style value from element.computedStyleMap

--- content.js
+++ content.js
@@ -19,6 +19,7 @@ Element.prototype.serializeWithStyles = (function() {
   // This list determines which css default values lookup tables are precomputed at load time
   // Lookup tables for other tag names will be automatically built at runtime if needed
   var tagNames = ['A', 'ABBR', 'ADDRESS', 'AREA', 'ARTICLE', 'ASIDE', 'AUDIO', 'B', 'BASE', 'BDI', 'BDO', 'BLOCKQUOTE', 'BODY', 'BR', 'BUTTON', 'CANVAS', 'CAPTION', 'CENTER', 'CITE', 'CODE', 'COL', 'COLGROUP', 'COMMAND', 'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN', 'DIV', 'DL', 'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FONT', 'FOOTER', 'FORM', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HEAD', 'HEADER', 'HGROUP', 'HR', 'HTML', 'I', 'IFRAME', 'IMG', 'INPUT', 'INS', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND', 'LI', 'LINK', 'MAP', 'MARK', 'MATH', 'MENU', 'META', 'METER', 'NAV', 'NOBR', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTION', 'OPTGROUP', 'OUTPUT', 'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RP', 'RT', 'RUBY', 'S', 'SAMP', 'SCRIPT', 'SECTION', 'SELECT', 'SMALL', 'SOURCE', 'SPAN', 'STRONG', 'STYLE', 'SUB', 'SUMMARY', 'SUP', 'SVG', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR', 'TRACK', 'U', 'UL', 'VAR', 'VIDEO', 'WBR'];
+  var styleKeys = Array.from(document.body.computedStyleMap().keys());
 
   // Precompute the lookup tables.
   for (var i = 0; i < tagNames.length; i++) {
@@ -37,10 +38,12 @@ Element.prototype.serializeWithStyles = (function() {
 
     var defaultStyle = {};
     var element = iframe.contentDocument.body.appendChild(document.createElement(tagName));
-    var computedStyle = getComputedStyle(element);
-    for (var i = 0; i < computedStyle.length; i++) {
-      defaultStyle[computedStyle[i]] = computedStyle[computedStyle[i]];
-    }
+    var styleMap = element.computedStyleMap();
+
+    styleKeys.forEach((styleKey) => {
+      defaultStyle[styleKey] = styleMap.get(styleKey).toString();
+    });
+
     return defaultStyle;
   }
 
@@ -69,25 +72,19 @@ Element.prototype.serializeWithStyles = (function() {
       var clonedE = clonedElements[i];
 
       if (!noStyleTags[e.tagName]) {
-        var computedStyle = getComputedStyle(e);
+        var styleMap = e.computedStyleMap();
         var defaultStyle = getDefaultStyleByTagName(e.tagName);
 
         cssTexts[i] = e.style.cssText;
 
-        for (var ii = 0; ii < computedStyle.length; ii++) {
-          var cssPropName = computedStyle[ii];
+        for (var ii = 0; ii < styleKeys.length; ii++) {
+          var cssPropName = styleKeys[ii];
+          var styleValue = styleMap.get(cssPropName).toString();
 
-          if (cssPropName === 'width' || cssPropName === 'height') {
-            var styleMap = e.computedStyleMap() || {};
-            var styleValue = styleMap.get(cssPropName) || {};
-
-            if (styleValue.value !== 'auto') {
-              clonedE.style[cssPropName] = computedStyle[cssPropName];
-            }
-          } else if (cssPropName === 'box-sizing') {
-            clonedE.style[cssPropName] = computedStyle[cssPropName];
-          } else if (computedStyle[cssPropName] !== defaultStyle[cssPropName]) {
-            clonedE.style[cssPropName] = computedStyle[cssPropName];
+          if (cssPropName === 'box-sizing') {
+            clonedE.style[cssPropName] = styleValue;
+          } else if (styleValue !== defaultStyle[cssPropName]) {
+            clonedE.style[cssPropName] = styleValue;
           }
         }
       }
