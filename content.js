a648c24bd5e264df959da25974960b203b8e1224
xiaoshuang.li <qq1157502759@gmail.com>
2019-12-26 17:50:40 +0800

    feat: 🎸 filter inherited style

--- content.js
+++ content.js
@@ -16,6 +16,7 @@ Element.prototype.serializeWithStyles = (function() {
   // This list determines which css default values lookup tables are precomputed at load time
   // Lookup tables for other tag names will be automatically built at runtime if needed
   const tagNames = ['A', 'ABBR', 'ADDRESS', 'AREA', 'ARTICLE', 'ASIDE', 'AUDIO', 'B', 'BASE', 'BDI', 'BDO', 'BLOCKQUOTE', 'BODY', 'BR', 'BUTTON', 'CANVAS', 'CAPTION', 'CENTER', 'CITE', 'CODE', 'COL', 'COLGROUP', 'COMMAND', 'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN', 'DIV', 'DL', 'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FONT', 'FOOTER', 'FORM', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HEAD', 'HEADER', 'HGROUP', 'HR', 'HTML', 'I', 'IFRAME', 'IMG', 'INPUT', 'INS', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND', 'LI', 'LINK', 'MAP', 'MARK', 'MATH', 'MENU', 'META', 'METER', 'NAV', 'NOBR', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTION', 'OPTGROUP', 'OUTPUT', 'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RP', 'RT', 'RUBY', 'S', 'SAMP', 'SCRIPT', 'SECTION', 'SELECT', 'SMALL', 'SOURCE', 'SPAN', 'STRONG', 'STYLE', 'SUB', 'SUMMARY', 'SUP', 'SVG', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR', 'TRACK', 'U', 'UL', 'VAR', 'VIDEO', 'WBR'];
+  const inheritedStyleKeys = ['azimuth', 'border-collapse', 'border-spacing', 'caption-side', 'color', 'cursor', 'direction', 'elevation', 'empty-cells', 'font-family', 'font-size', 'font-style', 'font-variant', 'font-weight', 'font', 'letter-spacing', 'line-height', 'list-style-image', 'list-style-position', 'list-style-type', 'list-style', 'orphans', 'pitch-range', 'pitch', 'quotes', 'richness', 'speak-header', 'speak-numeral', 'speak-punctuation', 'speak', 'speech-rate', 'stress', 'text-align', 'text-indent', 'text-transform', 'visibility', 'voice-family', 'volume', 'white-space', 'widows', 'word-spacing', 'text-size-adjust'];
   const bodyStyleMap = document.body.computedStyleMap();
   const styleKeys = Array.from(bodyStyleMap.keys());
 
@@ -84,7 +85,7 @@ Element.prototype.serializeWithStyles = (function() {
     const [clonedRoot] = clonedElements;
 
     elements.forEach((element, index) => {
-      const { tagName, style = {} } = element;
+      const { tagName, style = {}, parentElement } = element;
       const clonedElement = clonedElements[index];
       const noStyle = noStyleTags.includes(tagName);
 
@@ -93,6 +94,7 @@ Element.prototype.serializeWithStyles = (function() {
       }
 
       const styleMap = element.computedStyleMap();
+      const parentStyleMap = parentElement.computedStyleMap();
       const defaultStyle = getDefaultStyleByTagName(tagName);
 
       styleKeys.forEach((styleKey) => {
@@ -101,7 +103,16 @@ Element.prototype.serializeWithStyles = (function() {
         if (styleKey === 'box-sizing') {
           clonedElement.style[styleKey] = styleValue;
         } else if (styleValue !== defaultStyle[styleKey]) {
-          clonedElement.style[styleKey] = styleValue;
+          if (clonedRoot !== clonedElement) {
+            const inherited = inheritedStyleKeys.includes(styleKey);
+            const parentStyleValue = getStyleValueFromStyleMap(parentStyleMap, styleKey);
+
+            if (!inherited || parentStyleValue !== styleValue) {
+              clonedElement.style[styleKey] = styleValue; 
+            }
+          } else {
+            clonedElement.style[styleKey] = styleValue;
+          }
         }
       });
     });
