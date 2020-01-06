1c1855137cede60b1389f9c01ecff30f17874acb
xiaoshuang.li <qq1157502759@gmail.com>
2020-01-06 15:06:14 +0800

    feat: 🎸 convert src/href/background-image to right url

--- content.js
+++ content.js
@@ -13,6 +13,8 @@ Element.prototype.serializeWithStyles = (function() {
     'TITLE'
   ];
 
+  const urlAttributes = ['src', 'href'];
+
   // This list determines which css default values lookup tables are precomputed at load time
   // Lookup tables for other tag names will be automatically built at runtime if needed
   const tagNames = ['A', 'ABBR', 'ADDRESS', 'AREA', 'ARTICLE', 'ASIDE', 'AUDIO', 'B', 'BASE', 'BDI', 'BDO', 'BLOCKQUOTE', 'BODY', 'BR', 'BUTTON', 'CANVAS', 'CAPTION', 'CENTER', 'CITE', 'CODE', 'COL', 'COLGROUP', 'COMMAND', 'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN', 'DIV', 'DL', 'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FONT', 'FOOTER', 'FORM', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HEAD', 'HEADER', 'HGROUP', 'HR', 'HTML', 'I', 'IFRAME', 'IMG', 'INPUT', 'INS', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND', 'LI', 'LINK', 'MAP', 'MARK', 'MATH', 'MENU', 'META', 'METER', 'NAV', 'NOBR', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTION', 'OPTGROUP', 'OUTPUT', 'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RP', 'RT', 'RUBY', 'S', 'SAMP', 'SCRIPT', 'SECTION', 'SELECT', 'SMALL', 'SOURCE', 'SPAN', 'STRONG', 'STYLE', 'SUB', 'SUMMARY', 'SUP', 'SVG', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR', 'TRACK', 'U', 'UL', 'VAR', 'VIDEO', 'WBR'];
@@ -75,6 +77,12 @@ Element.prototype.serializeWithStyles = (function() {
     return [root, ...children];
   };
 
+  function getHref(str) {
+    const url = new URL(str, window.location.origin) || {};
+
+    return url.href
+  };
+
   return function serializeWithStyles() {
     if (this.nodeType !== Node.ELEMENT_NODE) {
       throw new TypeError();
@@ -90,6 +98,18 @@ Element.prototype.serializeWithStyles = (function() {
       const clonedElement = clonedElements[index];
       const noStyle = noStyleTags.includes(tagName);
 
+      urlAttributes.forEach((urlAttribute) => {
+        const value = clonedElement.getAttribute(urlAttribute);
+
+        if (!value) {
+          return;
+        }
+
+        const href = getHref(value);
+
+        clonedElement.setAttribute(urlAttribute, href);
+      });
+
       if (noStyle) {
         return;
       }
@@ -103,11 +123,21 @@ Element.prototype.serializeWithStyles = (function() {
 
         if (styleKey === 'box-sizing') {
           clonedElement.style[styleKey] = styleValue;
-        } else if (styleValue !== defaultStyle[styleKey]) {
-          if (clonedRoot !== clonedElement) {
+        }  else if (styleValue !== defaultStyle[styleKey]) {
+          if (styleKey === 'background-image') {
+            if (styleValue) {
+              clonedElement.style[styleKey] = '';
+              clonedElement.style[styleKey] = styleValue.replace(/url\(["']([^'"]*)["']\)/g, (value = '') => {
+                const baseUrl = value.slice(5, -2);
+                const href = getHref(baseUrl);
+
+                return `url("${href}")`;
+              });
+            }
+          } else if (clonedRoot !== clonedElement) {
             const inherited = inheritedStyleKeys.includes(styleKey);
             const parentStyleValue = getStyleValueFromStyleMap(parentStyleMap, styleKey);
-            const same = parentTagName === tagName && parentStyleValue === styleValue
+            const same = parentTagName === tagName && parentStyleValue === styleValue;
 
             if (!inherited || !same) {
               clonedElement.style[styleKey] = styleValue; 
@@ -119,6 +149,6 @@ Element.prototype.serializeWithStyles = (function() {
       });
     });
 
-    return clonedRoot.outerHTML;
+    return clonedRoot.outerHTML.replace(/&quot;/g, "'");
   }
 })();
